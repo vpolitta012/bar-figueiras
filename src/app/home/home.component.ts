0e64ac5acf2e09bc3c035c82220e6114dfc35dc9
vinipolitta <vinicius.politta1@gmail.com>
2023-11-15 20:01:00 -0300

    feat: ajuste nome variavel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent {
   img = "../../assets/img/CAPA-SITE-BAR-FIGUEIRAS.png"
 
-  teste() {
+  redirectToPdf() {
     const linkDeVisualizacao = "https://drive.google.com/file/d/1-_QIC-IbIyvTG6XTROs1ibKxS4HhEBx3/view?usp=drivesdk";
     window.open(linkDeVisualizacao, "_blank");
   }
