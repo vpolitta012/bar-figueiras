351c3fd21bb93adfed3058ef4854ee4a0d06bbb4
Victor Politta <150080141+vpolitta012@users.noreply.github.com>
2024-12-10 14:10:38 -0300

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent {
   img = "../../assets/img/CAPA-SITE-BAR-FIGUEIRAS.png"
 
   redirectToPdf() {
-    const linkDeVisualizacao = "https://drive.google.com/file/d/1-Rh36mbANNIVpu0OgtvolqPc3O7AQ3OR/";
+    const linkDeVisualizacao = "https://drive.google.com/file/d/1-Rh36mbANNIVpu0OgtvolqPc3O7AQ3OR/view?usp=sharing";
     window.open(linkDeVisualizacao, "_blank");
   }
 }
